9e2169b0b642c9ca1cadb4a39b163cc88f33f0e4
Tyler Watson <tylerww91@gmail.com>
2022-11-03 12:01:46 -0700

    adds test for /books should return a list of books

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -1,15 +1,31 @@
 const pool = require('../lib/utils/pool');
 const setup = require('../data/setup');
-// const request = require('supertest');
-// const app = require('../lib/app');
+const request = require('supertest');
+const app = require('../lib/app');
 
-describe('backend-express-template routes', () => {
+const { books } = require('../lib/books-data.js');
+
+describe('books routes', () => {
   beforeEach(() => {
     return setup(pool);
   });
-  it('example test - delete me!', () => {
-    expect(1).toEqual(1);
+
+  it('/books should return a list of books', async () => {
+    const res = await request(app).get('/books');
+    const expected = books.map((book) => {
+      return {
+        id: book.id,
+        title: book.title,
+        series: book.series,
+        author: book.author,
+        genre: book.genre,
+        url: book.url,
+        published: book.published,
+      };
+    });
+    expect(res.body).toEqual(expected);
   });
+
   afterAll(() => {
     pool.end();
   });
